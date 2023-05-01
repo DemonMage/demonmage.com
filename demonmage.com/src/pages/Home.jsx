fdf2e191c58affade3336d2201f89b545332b82c
Aaron <DemonMage@users.noreply.github.com>
2023-05-01 12:34:01 -0500

    Added basic content/styling to Home page

--- demonmage.com/src/pages/Home.jsx
+++ demonmage.com/src/pages/Home.jsx
@@ -1,4 +1,21 @@
+import PROFILE_PHOTO from "../assets/profilePhoto01.jpg";
+import "./Home.css";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <img src={PROFILE_PHOTO} />
+      <section>
+        <p>
+          Welcome! My name is Aaron and I'm a Software Engineer / Technical
+          Support Engineer. The kitty riding my shoulder is Crateos{" "}
+          <a href="https://ddowiki.com/page/Crateos" target="_blank">
+            (a silly double reference)
+          </a>
+          , an American Shorthair.
+        </p>
+      </section>
+    </div>
+  );
 }
 export default Home;
