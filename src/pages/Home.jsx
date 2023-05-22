4b63160c8162101ea5d853cb90b8386daab245ba
Aaron <DemonMage@users.noreply.github.com>
2023-05-22 16:57:52 -0500

    Updated home content and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,23 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="home">
-      <a href={PROFILE_PHOTO} target="_blank">
-        <img src={PROFILE_PHOTO} />
-      </a>
-      <section>
-        <p>
-          Welcome! My name is Aaron and I'm a Software Engineer / Technical
-          Support Engineer. The kitty riding my shoulder is Crateos (
-          <a href="https://ddowiki.com/page/Crateos" target="_blank">
-            a silly double reference
-          </a>
-          ), he's an American Shorthair. My hobbies include:
+    <>
+      <div className="home">
+        <a href={PROFILE_PHOTO} target="_blank">
+          <img src={PROFILE_PHOTO} />
+        </a>
+        <section>
+          <p>
+            Welcome! My name is Aaron and I'm a Software Engineer / Technical
+            Support Engineer. The kitty riding my shoulder is Crateos (
+            <a href="https://ddowiki.com/page/Crateos" target="_blank">
+              a silly double reference
+            </a>
+            ), he's an American Shorthair. My hobbies include:
+          </p>
           <ul>
             <li>
-              Video games especially RPGs like{" "}
+              Videos Games: Especially RPGs like{" "}
               <a
                 href="https://store.steampowered.com/app/435150/Divinity_Original_Sin_2__Definitive_Edition/"
                 target="_blank"
@@ -34,7 +36,7 @@ function Home() {
               !
             </li>
             <li>
-              Fantasy & Sci-Fi Reading good examples are anything by{" "}
+              Fantasy & Sci-Fi Reading: Good examples are anything by{" "}
               <a href="https://djangowexler.com/" target="_blank">
                 Django Wexler
               </a>
@@ -42,24 +44,28 @@ function Home() {
               <a href="https://rachelaaron.net/books.php" target="_blank">
                 Rachel Aaron
               </a>
-              ,{" "}
+              , or{" "}
               <a href="https://nkjemisin.com/writing/" target="_blank">
                 N.K. Jemisin
               </a>
             </li>
             <li>
-              Roleplaying games such as Pathfinder 2E,{" "}
+              Roleplaying Games: Such as{" "}
+              <a href="https://paizo.com/pathfinder" target="_blank">
+                Pathfinder 2E
+              </a>
+              , basically anything created by{" "}
               <a
                 href="https://sine-nomine-publishing.myshopify.com/"
                 target="_blank"
               >
-                Basically anything created by Kevin Crawford
+                Kevin Crawford
               </a>
             </li>
           </ul>
-        </p>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 }
 export default Home;
